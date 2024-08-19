71b3f73a2317d37a6839cd41abcab9da7e015f52
alexmncn <118453946+alexmncn@users.noreply.github.com>
2024-08-19 16:19:27 +0200

    Add short-by options

--- angular-app/src/app/shared/articles/articles.component.ts
+++ angular-app/src/app/shared/articles/articles.component.ts
@@ -1,13 +1,20 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
 
 import { ArticleComponent } from '../article/article.component';
 import { Article } from '../../models/article.model';
 
+interface SortByOption {
+  name: string;
+  code: number;
+}
+
 @Component({
   selector: 'app-articles',
   standalone: true,
-  imports: [CommonModule, ArticleComponent],
+  imports: [CommonModule, ArticleComponent, FormsModule, DropdownModule], 
   templateUrl: './articles.component.html',
   styleUrl: './articles.component.css'
 })
@@ -20,6 +27,21 @@ export class ArticlesComponent {
   gridDisplay: boolean = true;
   listDisplay: boolean = false;
   placeholders: Article[] = new Array(this.per_page).fill('');
+
+  sortOptions: SortByOption[] = [
+    { name: 'Precio: de menor a mayor', code: 0 },
+    { name: 'Precio: de mayor a menor', code: 1 },
+    { name: 'Nombre: A a Z', code: 2 },
+    { name: 'Nombre: Z a A', code: 3 }
+  ];
+
+  selectedSort: SortByOption | undefined;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes["articles"]) {
+      this.sortArticles();
+    }
+  }
 
   load(): void {
     this.loadArticles.emit();
@@ -40,6 +62,25 @@ export class ArticlesComponent {
     if (this.listDisplay == false) {
       this.listDisplay = true;
       this.gridDisplay = false;
+    }
+  }
+
+  sortArticles() {
+    switch (this.selectedSort?.code) {
+      case 0:
+        this.articles.sort((a, b) => a.pvp - b.pvp);
+        break;
+      case 1:
+        this.articles.sort((a, b) => b.pvp - a.pvp);
+        break; 
+      case 2:
+        this.articles.sort((a, b) => a.detalle.localeCompare(b.detalle));
+        break;
+      case 3:
+        this.articles.sort((a, b) => b.detalle.localeCompare(a.detalle));
+        break;
+      default:
+        break;
     }
   }
 
