c4e6c40c1772ea2fdeeeeb05be3e142e7862ea92
alexmncn <118453946+alexmncn@users.noreply.github.com>
2024-10-01 19:09:08 +0200

    Improvements admin routes.

--- flask-app/app/routes/admin.py
+++ flask-app/app/routes/admin.py
@@ -1,13 +1,13 @@
 from flask import Blueprint, request, jsonify
-from flask_jwt_extended import jwt_required, get_jwt, get_jwt_identity
+from flask_jwt_extended import jwt_required
 
 from app.services import admin_data
 
 
 admin_bp = Blueprint('admin', __name__)
 
-
 @admin_bp.route('/articles/feature', methods=['POST'])
+@jwt_required()
 def feature_article():
     codebar = request.args.get('codebar', None)
     
@@ -15,10 +15,10 @@ def feature_article():
         status = admin_data.feature_article(codebar)
         
         if status == True:
-            return jsonify(message=f'Article "{codebar}" featured successfully.'), 200
+            return jsonify(message=f'Article with codebar `{codebar}` featured successfully.'), 200
+        elif status == False:
+            return jsonify(error=f'No articles with codebar `{codebar}` in database.'), 404
         
-        return jsonify(message='Error'), 500
+        return jsonify(error='Internal error.'), 500
     
-    return jsonify(message='No codebar'), 400
-    
-    
+    return jsonify(error='No codebar in request.'), 400
--- flask-app/app/services/admin_data.py
+++ flask-app/app/services/admin_data.py
@@ -1,15 +1,21 @@
-
 from app.extensions import db
 from app.models import Article
-
+from app.services.pushover_alerts import send_alert
 
 def feature_article(codebar):
-    session = db.session
-    n_updates = session.query(Article).filter(Article.codebar==codebar).update({Article.destacado: True})
-    
-    if n_updates == 1:
-        session.commit()
-        return True
-    
-    session.close()
-    return False
+    try:    
+        session = db.session
+        updated_rows = session.query(Article).filter(Article.codebar==codebar).update({Article.destacado: True})
+        
+        if updated_rows == 1:
+            session.commit()
+            session.close()
+            
+            send_alert(f'Se ha destacado el articulo con <b>codebar: {codebar}</b>', -1)
+            return True 
+        
+        session.close()
+        return False
+    except Exception as e:
+        send_alert(f'Error al destacar el articulo con <b>codebar: {codebar}</b>:\n {e}', 0)
+        return e
