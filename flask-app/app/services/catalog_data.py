69c863477442e970459ab69d58631ae9c96d8d35
alexmncn <118453946+alexmncn@users.noreply.github.com>
2024-10-18 14:43:28 +0200

    Add global filters to querys. Support hidden prop.
    Not finish

--- flask-app/app/services/catalog_data.py
+++ flask-app/app/services/catalog_data.py
@@ -1,20 +1,22 @@
 import datetime
-from sqlalchemy import desc,text
+from sqlalchemy import desc, text
 
 from app.extensions import db
 from app.models import Article, Family
 
 new_articles_range = 30 # in days
 
+def_article_filter = [Article.hidden == False]
+def_family_filter = [Family.hidden == False]
 
 def all_articles_total():
-    return Article.query.count()
+    return Article.query.filter(*def_article_filter).count()
 
 
 def all_articles(page, per_page):
     offset = (page - 1) * per_page
     
-    articles = Article.query.limit(per_page).offset(offset).all()
+    articles = Article.query.filter(*def_article_filter).limit(per_page).offset(offset).all()
     return [article.to_dict_reduced() for article in articles]
 
 
@@ -50,44 +52,41 @@ def search_articles_total(search):
 
 
 def featured_articles_total():
-    return Article.query.filter_by(destacado=1).count()
+    return Article.query.filter(*def_article_filter).filter_by(destacado=1).count()
 
 
 def featured_articles(page, per_page):
     offset = (page - 1) * per_page
     
-    articles = Article.query.filter_by(destacado=1).limit(per_page).offset(offset)
+    articles = Article.query.filter(*def_article_filter).filter_by(destacado=1).limit(per_page).offset(offset)
     return [article.to_dict_reduced() for article in articles]
 
 
 def new_articles_total():
     time_threshold = datetime.datetime.now(datetime.UTC) - datetime.timedelta(days=new_articles_range)
     
-    return Article.query.filter(Article.factualizacion>=time_threshold).order_by(desc(Article.factualizacion)).count()
+    return Article.query.filter(*def_article_filter, Article.factualizacion>=time_threshold).order_by(desc(Article.factualizacion)).count()
 
 
 def new_articles(page, per_page):
     offset = (page - 1) * per_page
     time_threshold = datetime.datetime.now(datetime.UTC) - datetime.timedelta(days=new_articles_range)
     
-    articles = Article.query.filter(Article.factualizacion>=time_threshold).order_by(desc(Article.factualizacion)).limit(per_page).offset(offset)
-    
-    if articles is None:
-        articles = Article.query.all().order_by(desc(Article.factualizacion)).limit(per_page)
+    articles = Article.query.filter(*def_article_filter).all().order_by(desc(Article.factualizacion)).limit(per_page)
     
     return [article.to_dict_reduced() for article in articles]
     
 
 def all_families():
-    families = Family.query.all()
+    families = Family.query.filter(*def_family_filter).all()
     return [family.to_dict_reduced() for family in families]
 
 
 def family_articles(family_id, page, per_page):
     offset = (page - 1) * per_page
     
-    articles = Article.query.filter_by(codfam=family_id).limit(per_page).offset(offset)
+    articles = Article.query.filter(*def_article_filter).filter_by(codfam=family_id).limit(per_page).offset(offset)
     return [article.to_dict_reduced() for article in articles]
 
 def family_articles_total(family_id):
-    return Article.query.filter_by(codfam=family_id).count()
+    return Article.query.filter(*def_article_filter).filter_by(codfam=family_id).count()
