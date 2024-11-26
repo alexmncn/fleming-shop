736c4477b20a55d3cbc57f1253b8a1743cd8117f
alexmncn <118453946+alexmncn@users.noreply.github.com>
2024-11-26 16:29:43 +0100

    Add security to hide routes. Add recursive option

--- flask-app/app/routes/admin.py
+++ flask-app/app/routes/admin.py
@@ -55,10 +55,11 @@ def hide_article():
 
 
 @admin_bp.route('/families/hide', methods=['POST'])
-
+@jwt_required()
 def hide_family():
     codfam = request.args.get('codfam', None, int)
     hidden = request.args.get('hidden', 'true') in ['True','true', '1', 'yes', 'y']
+    recursive = request.args.get('recursive', 'true') in ['True','true', '1', 'yes', 'y']
     
     if codfam:
         status = admin_data.hide_family(codfam, hidden)
@@ -68,6 +69,16 @@ def hide_family():
                 message = f'Family with code `{codfam}` successfully hidden.'
             else:
                 message = f'Family with code `{codfam}` removed from hidden.'
+                
+            if recursive == True:
+                # Llamada interna a la función que oculta los artículos
+                articles_status = admin_data.hide_family_articles(codfam, hidden)
+                
+                if articles_status == True:
+                    message += f' Articles from family `{codfam}` successfully hidden.' if hidden else f' Articles from family `{codfam}` removed from hidden.'
+                else:
+                    return jsonify(error=f'Failed to hide articles for family with code `{codfam}`.'), 500
+                
             return jsonify(message=message), 200
         
         elif status == False:
@@ -77,9 +88,8 @@ def hide_family():
         
     return jsonify(error='No codebar in request.'), 400
 
-
 @admin_bp.route('/family/articles/hide', methods=['POST'])
-
+@jwt_required()
 def hide_family_articles():
     codfam = request.args.get('codfam', None, int)
     hidden = request.args.get('hidden', 'true') in ['True','true', '1', 'yes', 'y']
