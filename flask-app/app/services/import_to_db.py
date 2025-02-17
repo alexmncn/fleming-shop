ebb4da15fed8818d0481b0c3accc08b6dbf63348
alexmncn <118453946+alexmncn@users.noreply.github.com>
2025-02-18 00:52:14 +0100

    Working imports logs.

--- flask-app/app/services/import_to_db.py
+++ flask-app/app/services/import_to_db.py
@@ -155,176 +155,239 @@ def validate_clean_article_data(row):
     return True, row, fixes
 
 
-def update_articles(articles_dbf, username):
+def update_articles(articles_dbf, user):
+    start_time = time.perf_counter() # Inicializar contador de tiempo
+    
     articles_csv_path = articles_dbf_to_csv(articles_dbf) # DBF to CSV
     import_id = str(uuid.uuid4()) # Genera uuid de importación
-    
+    status = 0
+    status_info = ''
+        
     time.sleep(2) # Espera que el archivo csv generado este disponible !!!provisional!!!
 
     # Leer los datos del CSV
+    articles_csv = None
     try:
         articles_csv = pd.read_csv(articles_csv_path)
     except Exception as e:
-        print(f"Error al leer el CSV: {str(e)}")
-        return
+        status = 1
+        status_info = f"Error al leer el CSV: {str(e)}"
+        print(status_message)
     
-    
-    # Limpiamos los datos del CSV
-    clean_rows = []
-    conflict_logs = []
-
-    for _, row in articles_csv.iterrows():
-        codebar = row.get('CCODEBAR')
-        ref = row.get('CREF')
-        detalle = row.get('CDETALLE')
-        # Si no hay código de barras, se descarta directamente
-        if pd.isna(codebar):
-            log_type = 2 # No CODEBAR
-            log_info = 'Código de barras ausente'
-            article_import_log = Article_import_log(import_id=import_id, type=log_type, ref=ref, codebar=codebar, detalle=detalle, info='Código de barras ausente')
-            conflict_logs.append(article_import_log)
-            continue
-        
-        # Validar y limpiar los datos de la fila
-        is_valid, clean_row, logs_info = validate_clean_article_data(row)
-        if not is_valid:
-            for log_info in logs_info:
-                log_type = 1 # NO Válido
-                article_import_log = Article_import_log(import_id=import_id, type=log_type, ref=ref, codebar=codebar, detalle=detalle, info=log_info)
-                conflict_logs.append(article_import_log)
-            continue
-        else:
-            for log_info in logs_info:
-                log_type = 0 # Válido
-                article_import_log = Article_import_log(import_id=import_id, type=log_type, ref=ref, codebar=codebar, detalle=detalle, info=log_info)
-                conflict_logs.append(article_import_log)
-        
-        # Si es valida se añade a la nueva lista
-        clean_rows.append(clean_row)
-        
-    # Crear un nuevo DataFrame con las filas limpias
-    clean_articles_csv = pd.DataFrame(clean_rows, columns=articles_csv.columns)
-    
-    
-    # Identificar filas duplicadas
-    duplicated_rows = clean_articles_csv[clean_articles_csv.duplicated(subset=['CCODEBAR'], keep='first')]
-    for _, row in duplicated_rows.iterrows():
-        log_type = 3 # DUPLICADO
-        log_info = 'Código de barras duplicado'
-        article_import_log = Article_import_log(import_id=import_id, type=log_type, ref=row['CREF'], codebar=row['CCODEBAR'], detalle=row['CDETALLE'], info=log_info)
-        conflict_logs.append(article_import_log)
-        
-    # Eliminar duplicados, manteniendo solo la primera ocurrencia
-    clean_articles_csv = clean_articles_csv.drop_duplicates(subset=['CCODEBAR'], keep='first')
-        
-    
-    # Cargamos todos los artículos existentes en la DB
-    db_articles = {article.codebar: article for article in Article.query.all()}
-    deleted_articles = set(db_articles.keys()) # Inicializamos los artículos a eliminar
-    
-    # Actualizar o crear los artículos en la DB
+    # Inicializar las que sirven para el log 
+    # (Aunque de error general y esten vacías, poder guardar el import_log general)
     new_articles = []
     updated_articles = []
+    deleted_articles = []
+    duplicated_rows = []
+    conflict_logs = []
     
-    for _, article_csv in clean_articles_csv.iterrows():
-        codebar = article_csv['CCODEBAR']
-        
-        # Si el artículo ya existe, se actualiza
-        if codebar in db_articles:
-            article_db = db_articles[codebar] # Obtener el artículo de la lista de la DB
-            deleted_articles.remove(codebar) # Eliminar de la lista de articulos a eliminar
+    # Si se ha leído correctamente
+    if articles_csv is not None and status == 0:
+        try:
+            clean_rows = []
             
-            changes = {}
-            for col, attr in article_column_to_attribute_map.items():
-                if col != 'CCODEBAR': # Filtramos el codebar (simpre coincidirán)
-                    new_value = article_csv[col] # Valor 'nuevo' del CSV
-                    db_value = getattr(article_db, attr) # Valor actual en la DB
-                    
-                    # Convertir la fecha a string sin la hora, para comparar con el CSV
-                    if attr == 'factualizacion':
-                        db_value = db_value.strftime('%Y-%m-%d')
-                        
-                    # Si cambia el valor, se guarda en 'changes'
-                    if new_value != db_value:
-                        changes[attr] = new_value
-                    
-            # Si hay cambios, se actualiza el artículo y se guarda en la lista de actualizados
-            if changes:
-                for attr, new_value in changes.items():
-                    setattr(article_db, attr, new_value)
-                updated_articles.append(article_db)
+            # Limpiamos los datos del CSV
+            for _, row in articles_csv.iterrows():
+                codebar = row.get('CCODEBAR')
+                ref = row.get('CREF')
+                detalle = row.get('CDETALLE')
+                # Si no hay código de barras, se descarta directamente
+                if pd.isna(codebar):
+                    log_type = 2 # No CODEBAR
+                    log_info = 'Código de barras ausente'
+                    article_import_log = Article_import_log(import_id=import_id, type=log_type, ref=ref, codebar=None, detalle=detalle, info='Código de barras ausente')
+                    conflict_logs.append(article_import_log)
+                    continue
                 
-        else:
-            # Inserta como nuevo artículo 
-            article = Article(
-                ref=article_csv['CREF'],
-                detalle=article_csv['CDETALLE'],
-                codfam=article_csv['CCODFAM'],
-                pcosto=article_csv['NCOSTEDIV'],
-                pvp=article_csv['NPVP'],
-                codebar=article_csv['CCODEBAR'],
-                factualizacion=article_csv['DACTUALIZA']
-            )
-            new_articles.append(article)
-        
-    # Guardar los cambios en la DB
-    session = db.session
-        
-    # Aplicar cambios en la base de datos
-    try:
-        session.add_all(new_articles) # Insertar los nuevos artículos
-        session.add_all(updated_articles) # Actualizar los artículos modificados 
+                # Validar y limpiar los datos de la fila
+                is_valid, clean_row, logs_info = validate_clean_article_data(row)
+                if not is_valid:
+                    for log_info in logs_info:
+                        log_type = 1 # NO Válido
+                        article_import_log = Article_import_log(import_id=import_id, type=log_type, ref=ref, codebar=codebar, detalle=detalle, info=log_info)
+                        conflict_logs.append(article_import_log)
+                    continue
+                else:
+                    for log_info in logs_info:
+                        log_type = 0 # Válido
+                        article_import_log = Article_import_log(import_id=import_id, type=log_type, ref=ref, codebar=codebar, detalle=detalle, info=log_info)
+                        conflict_logs.append(article_import_log)
+                
+                # Si es valida se añade a la nueva lista
+                clean_rows.append(clean_row)
+                
+            # Crear un nuevo DataFrame con las filas limpias
+            clean_articles_csv = pd.DataFrame(clean_rows, columns=articles_csv.columns)
+            
+            
+            # Identificar filas duplicadas
+            duplicated_rows = clean_articles_csv[clean_articles_csv.duplicated(subset=['CCODEBAR'], keep='first')]
+            for _, row in duplicated_rows.iterrows():
+                log_type = 3 # DUPLICADO
+                log_info = 'Código de barras duplicado'
+                article_import_log = Article_import_log(import_id=import_id, type=log_type, ref=row['CREF'], codebar=row['CCODEBAR'], detalle=row['CDETALLE'], info=log_info)
+                conflict_logs.append(article_import_log)
+                
+            # Eliminar duplicados, manteniendo solo la primera ocurrencia
+            clean_articles_csv = clean_articles_csv.drop_duplicates(subset=['CCODEBAR'], keep='first')
+                
+            
+            # Cargamos todos los artículos existentes en la DB
+            db_articles = {article.codebar: article for article in Article.query.all()}
+            deleted_articles = set(db_articles.keys()) # Inicializamos los artículos a eliminar
+            
+            # Actualizar o crear los artículos en la DB
+            new_articles = []
+            updated_articles = []
+            
+            for _, article_csv in clean_articles_csv.iterrows():
+                codebar = article_csv['CCODEBAR']
+                
+                # Si el artículo ya existe, se actualiza
+                if codebar in db_articles:
+                    article_db = db_articles[codebar] # Obtener el artículo de la lista de la DB
+                    deleted_articles.remove(codebar) # Eliminar de la lista de articulos a eliminar
+                    
+                    changes = {}
+                    for col, attr in article_column_to_attribute_map.items():
+                        if col != 'CCODEBAR': # Filtramos el codebar (simpre coincidirán)
+                            new_value = article_csv[col] # Valor 'nuevo' del CSV
+                            db_value = getattr(article_db, attr) # Valor actual en la DB
+                            
+                            # Convertir la fecha a string sin la hora, para comparar con el CSV
+                            if attr == 'factualizacion':
+                                db_value = db_value.strftime('%Y-%m-%d')
+                                
+                            # Si cambia el valor, se guarda en 'changes'
+                            if new_value != db_value:
+                                changes[attr] = new_value
+                            
+                    # Si hay cambios, se actualiza el artículo y se guarda en la lista de actualizados
+                    if changes:
+                        for attr, new_value in changes.items():
+                            setattr(article_db, attr, new_value)
+                        updated_articles.append(article_db)
+                        
+                else:
+                    # Inserta como nuevo artículo 
+                    article = Article(
+                        ref=article_csv['CREF'],
+                        detalle=article_csv['CDETALLE'],
+                        codfam=article_csv['CCODFAM'],
+                        pcosto=article_csv['NCOSTEDIV'],
+                        pvp=article_csv['NPVP'],
+                        codebar=article_csv['CCODEBAR'],
+                        factualizacion=article_csv['DACTUALIZA']
+                    )
+                    new_articles.append(article)
+                
+            # Aplicar cambios en la base de datos
+            try:
+                session = db.session
+                session.add_all(new_articles) # Insertar los nuevos artículos
+                session.add_all(updated_articles) # Actualizar los artículos modificados 
 
-        for codebar in deleted_articles:
-            session.delete(db_articles[codebar]) # Eliminar los artículos que ya no existen
-             
-        session.commit()
-    except SQLAlchemyError as e:
-        session.rollback()
-        print(f"Error en la base de datos: {str(e)}")
-    finally:
-        session.close()
+                for codebar in deleted_articles:
+                    session.delete(db_articles[codebar]) # Eliminar los artículos que ya no existen
+                    
+                session.commit() # Confirmar los cambios
+                
+                status_info = "Importación completada con éxito."
+            except SQLAlchemyError as e:
+                session.rollback()
+                status = 1
+                status_info = f"Error en la base de datos: {str(e)}"
+            finally:
+                session.close()
+                
+        except Exception as e:
+            status = 1
+            status_info = f"Error inesperado procesando los datos: {str(e)}"
         
         
-    # LOGS
-    user_id = User.query(username=username).first().id
+    # Calcular tiempo de ejecución
+    end_time = time.perf_counter()
+    elapsed_time = (end_time - start_time)
     
     # Log temp
     status_message = f"Nuevos: {len(new_articles)}, Actualizados: {len(updated_articles)}, Eliminados: {len(deleted_articles)}, Duplicados: {len(duplicated_rows)}, Errores: {len(conflict_logs)}"
+    print(status_message)
     
-    if conflict_logs:
-        # Instance article_import object with import info
+    # Save LOGs
+    save_article_import_log(
+        import_id, 
+        user, 
+        status, 
+        status_info, 
+        len(new_articles), 
+        len(updated_articles), 
+        len(deleted_articles), 
+        len(duplicated_rows), 
+        conflict_logs, 
+        elapsed_time, 
+        articles_csv_path
+    ) 
+        
+    return status, status_info
+        
+        
+def save_article_import_log(import_id, user, status, info, n_new, n_updated, n_deleted, n_duplicated, conflict_logs, elapsed_time, articles_csv_path):
+    # Get user id of the user who made the import
+    user = User.query.filter(User.username==user['username']).first()
+    user_id = user.id
+    
+    if user_id is not None:
+        # Create article_import object with import info
         article_import = Article_import(
             id=import_id, 
-            user_id=user_id, 
-            ew_rows=len(new_articles), 
-            updates_rows=len(updated_articles), 
-            deleted_rows=len(deleted_articles), 
-            duplicated_rows=len(duplicated_rows), 
-            errors=len(conflict_logs), 
-            status=0, 
-            info='Exito'
+            user_id=user_id,
+            status=status,
+            info=info,
+            new_rows=n_new, 
+            updated_rows=n_updated, 
+            deleted_rows=n_deleted, 
+            duplicated_rows=n_duplicated,
+            errors=len(conflict_logs),
+            elapsed_time=elapsed_time
         )
         
-        # Save conflict log csv
-        log_file_path = os.path.join(DATA_LOGS_ROUTE, f"conflicts_{os.path.basename(articles_csv_path)}")
-        with open(log_file_path, mode='w', newline='', encoding='utf-8') as log_file:
-            log_writer = pd.DataFrame(conflict_logs)
-            log_writer.to_csv(log_file, index=False)
+        # Save logs in the DB
+        try:
+            session = db.session
             
-        # Delete old conflict logs
-        pattern = os.path.join(f"{str.split(log_file_path,'_')[0]}_{str.split(log_file_path,'_')[1]}_*.csv") # Get old file versions
-
-        for file_ in glob.glob(pattern):
-            file_name = os.path.basename(file_)
-            new_file_name = os.path.basename(log_file_path)
-
-            if file_name != new_file_name: # Remove all except the new one
-                os.remove(file_)
-                
-        print(f"Conflictos guardados en {log_file_path}")
+            # Save article import first, then save the logs associated
+            session.add(article_import)
+            session.commit()
+            
+            # Save conflict logs
+            session.add_all(conflict_logs)
+            session.commit()
+            
+            print(f"Se han guardado los logs en la DB")
+        except SQLAlchemyError as e:
+            session.rollback()
+            print(f"Error al guardar logs de la importación en la DB: {str(e)}")
+    
+    
+    # Save logs in CSV file
+    log_file_path = os.path.join(DATA_LOGS_ROUTE, f"conflicts_{os.path.basename(articles_csv_path)}")
+    with open(log_file_path, mode='w', newline='', encoding='utf-8') as log_file:
+        log_writer = pd.DataFrame(conflict_logs)
+        log_writer.to_csv(log_file, index=False)
         
-    print(status_message)
+    # Delete old conflict logs
+    pattern = os.path.join(f"{str.split(log_file_path,'_')[0]}_{str.split(log_file_path,'_')[1]}_*.csv") # Get old file versions
+
+    for file_ in glob.glob(pattern):
+        file_name = os.path.basename(file_)
+        new_file_name = os.path.basename(log_file_path)
+
+        if file_name != new_file_name: # Remove all except the new one
+            os.remove(file_)
+            
+    print(f"Conflictos guardados en {log_file_path}")
+            
     
 
 def families_dbf_to_csv(families_dbf):
