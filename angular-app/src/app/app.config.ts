323134f748bc473b0b7a1b6b998943d5a07a91b6
alexmncn <alejandro10montes@gmail.com>
2025-03-24 13:37:36 +0100

    Fix primeng

--- angular-app/src/app/app.config.ts
+++ angular-app/src/app/app.config.ts
@@ -4,15 +4,23 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AuthInterceptor } from './services/auth/auth.interceptor';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
     provideHttpClient(),
-    provideHttpClient(
-      withInterceptors([AuthInterceptor])
-    ),
-    provideAnimationsAsync()
+    provideHttpClient(withInterceptors([AuthInterceptor])),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: false || 'none'
+        }
+      }
+    })
   ]
 };
