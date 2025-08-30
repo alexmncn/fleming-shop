ad41e285b6e2726ea26e9180cc88aa782556f888
Alex <alejandro10montes@gmail.com>
2025-08-30 22:02:21 +0200

    Improve data-export section

--- angular-app/src/app/features/admin/data-export/data-export.component.ts
+++ angular-app/src/app/features/admin/data-export/data-export.component.ts
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './data-export.component.css'
 })
 export class DataExportComponent {
+  formatOptions = [
+    { value: 'pdf', viewValue: 'PDF', icon: 'pi pi-file-pdf' },
+    { value: 'csv', viewValue: 'CSV', icon: 'pi pi-file-o' },
+    { value: 'xlsx', viewValue: 'EXCEL', icon: 'pi pi-file-excel' },
+    { value: 'dbf', viewValue: 'DBF', icon: 'pi pi-database' }
+  ];
 
+  downloadFile() {
+
+  }
 }
