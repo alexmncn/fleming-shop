2090fd97dff4bde0ee8499384495c35103c8c7fa
alexmncn <alejandro10montes@gmail.com>
2025-10-30 00:11:32 +0100

    Fix: scroll reset in navigation

--- angular-app/src/app/app.config.ts
+++ angular-app/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -10,8 +10,12 @@ import Aura from '@primeng/themes/aura';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes),
-    provideHttpClient(),
+    provideRouter(routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top',
+        anchorScrolling: 'enabled',
+      })
+    ),
     provideHttpClient(withInterceptors([AuthInterceptor])),
     provideAnimationsAsync(),
     providePrimeNG({
--- angular-app/src/app/features/catalog/catalog.component.ts
+++ angular-app/src/app/features/catalog/catalog.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 
 import { NavBarComponent } from "../../shared/nav-bar/nav-bar.component";
 import { FloatingMessageComponent } from "../../shared/floating-message/floating-message.component";
+import { filter } from 'rxjs';
 
 @Component({
     selector: 'app-catalog',
@@ -11,5 +12,13 @@ import { FloatingMessageComponent } from "../../shared/floating-message/floating
     styleUrl: './catalog.component.css'
 })
 export class CatalogComponent {
-
+    constructor(private router: Router) {
+    this.router.events.pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd))
+    .subscribe(() => {
+        setTimeout(() => {
+        const container = document.querySelector('.main-content');
+        container?.scrollTo({ top: 0 });
+        }, 0);
+    });
+  }
 }
