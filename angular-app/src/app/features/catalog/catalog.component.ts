8bc8595a7b1f8150a6498acdc542807711286cb6
Alex <alejandro10montes@gmail.com>
2025-12-06 22:42:49 +0100

    Update catalog.component.ts

--- angular-app/src/app/features/catalog/catalog.component.ts
+++ angular-app/src/app/features/catalog/catalog.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 
-import { NavBarComponent } from "../../shared/nav-bar/nav-bar.component";
 import { FloatingMessageComponent } from "../../shared/floating-message/floating-message.component";
 import { filter } from 'rxjs';
 
 @Component({
     selector: 'app-catalog',
-    imports: [RouterOutlet, NavBarComponent, FloatingMessageComponent],
+    imports: [RouterOutlet, FloatingMessageComponent],
     templateUrl: './catalog.component.html',
     styleUrl: './catalog.component.css'
 })
